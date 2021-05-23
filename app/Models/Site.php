a50202f3f9da9b86b22759ff89a919f1d143af7f
allexgalbert <allexgalbert@gmail.com>
2021-05-23 16:15:18 +0300

    update reviewlicms
    replace database query from CatalogController.php to model Site.php in method getList()

--- app/Models/Site.php
+++ app/Models/Site.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Site extends Model {
 
@@ -46,5 +47,85 @@ class Site extends Model {
   //sites -> reviews
   public function reviews() {
     return $this->hasMany(Review::class);
+  }
+
+  public static function getList($request) {
+
+    $sites = self::query();
+    $sites->select('sites.*');
+
+    //категория
+    if ($request->category_id) {
+      $sites
+        ->join('categories', 'categories.id', '=', 'sites.category_id')
+        ->where('categories.id', $request->category_id);
+    }
+
+    //бренд
+    if ($request->brand_id) {
+      $sites
+        ->join('brand_site', 'brand_site.site_id', '=', 'sites.id')
+        ->where('brand_site.brand_id', $request->brand_id);
+    }
+
+    //особенность
+    if ($request->feature_id) {
+      $sites
+        ->join('feature_site', 'feature_site.site_id', '=', 'sites.id')
+        ->where('feature_site.feature_id', $request->feature_id);
+    }
+
+    //колво отзывов
+    $sites->withCount('reviews');
+
+    //(select count(*) from `reviews` where `sites`.`id` = `reviews`.`site_id`) as `reviews_count`
+
+    //по колву отзывов
+    if ($request->reviews_count) {
+      $sites->orderBy('reviews_count', 'desc');
+    }
+
+    //сумма по -1 и +1
+    $sites->withSum('reviews', 'rating');
+
+    //(select sum(`reviews`.`rating`) from `reviews` where `sites`.`id` = `reviews`.`site_id`) as `reviews_sum_rating`
+
+
+    //сумма по -1
+    $sites->withCount([
+      'reviews AS ratingMinus' => function ($query) {
+        $query->select(DB::raw("SUM(rating)"))->where('rating', -1);
+      }
+    ]);
+
+    //(select SUM(rating) from `reviews` where `sites`.`id` = `reviews`.`site_id` and `rating` = ?) as `ratingMinus`
+
+
+    //сумма по +1
+    $sites->withCount([
+      'reviews AS ratingPlus' => function ($query) {
+        $query->select(DB::raw("SUM(rating)"))->where('rating', 1);
+      }
+    ]);
+
+    //(select SUM(rating) from `reviews` where `sites`.`id` = `reviews`.`site_id` and `rating` = ?) as `ratingPlus`
+
+    //всего оценок не 0
+    $sites->withCount([
+      'reviews AS ratingPlusMinus' => function ($query) {
+        $query->select(DB::raw("COUNT(rating)"))->where('rating', '!=', 0);
+      }
+    ]);
+
+    //по рейтингу
+    if ($request->reviews_sum_rating) {
+      $sites->orderBy('reviews_sum_rating', 'desc');
+    }
+
+    //dump($sites->dd());
+    //Brand::query()->explain()->dd();
+    //dump($sites->explain());
+
+    return $sites;
   }
 }
